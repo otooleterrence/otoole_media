8821db8c524502f5fb2a1ff5c26575a8aadc5b9c
Terry O'Toole <otooleterrence@gmail.com>
2018-05-02 22:36:01 -0400

    turn off service workers

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import React from 'react';
 import { render } from 'react-snapshot';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 render(
   <App />,
   document.getElementById('root')
 );
-registerServiceWorker();
+// registerServiceWorker();
